59ac53a766f0e4a3c958393a2470c69b160dd59a
Vatsalya Goel <vatsalyagoel@Macs-MacBook-Pro.local>
2015-08-15 21:04:40 +1000

    testing import probs

--- musicStreaming/index.php
+++ musicStreaming/index.php
@@ -53,12 +53,19 @@
             button.text("PLAY ME!!!");
             button.attr("onclick", "playMusic()");
         }
+        
+        function playMusic2(){
+            console.log("Am Playing Now!!!");
+            var button = $("#play");
+            button.text("Pause ME!!!");
+            button.attr("onclick", "pauseMusic()");
+        }
     </script>
 
 </head>
 
 <body>
-    <audio id="player" controls="controls">
+    <audio id="player" controls="controls" onplay="playMusic2();">
         <source src="Macklemore-and-Ryan-Lewis-Growing-Up-Sloanes-Song-feat-Ed-Sheeran.mp3" type="audio/mpeg">
         Your browser does not support the audio element.
     </audio>
