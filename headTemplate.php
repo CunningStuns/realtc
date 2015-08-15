54d120a671e7ecbdfccf74514d73f109c115cd4f
Vatsalya Goel <vatsalyagoel@Macs-MacBook-Pro.local>
2015-08-16 09:43:03 +1000

    Head Template Fix

--- headTemplate.php
+++ headTemplate.php
@@ -8,7 +8,11 @@ if ($style){
 	include realpath(dirname(__FILE__)).$style;
 	echo "</style>";
 }
-echo" <meta name='google-signin-client_id' content='251993177722-sm7r5qj9sq3lrff1ao2hgeoesmjs89dj.apps.googleusercontent.com'>
+echo" 
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css'>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
+    <meta name='google-signin-client_id' content='251993177722-sm7r5qj9sq3lrff1ao2hgeoesmjs89dj.apps.googleusercontent.com'>
 </head>
 ";
 }
