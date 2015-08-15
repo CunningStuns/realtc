28bd58ec00c9c5cd5e14b41174732623967dace0
Vatsalya Goel <vatsalyagoel@Macs-MacBook-Pro.local>
2015-08-16 02:43:30 +1000

    update sudio player

--- login/loginjs.php
+++ login/loginjs.php
@@ -13,11 +13,11 @@
             console.log("here");
             var selector = $("div#aboutBox" );
             selector.css("display", "inline");
-            console.log($("div#aboutBox"));
+            console.log($("div#aboutBox").style);
             console.log(document.getElementById('#aboutBox').style);
             console.log(document.getElementById('#aboutBox').style.display);
             console.log($("div#aboutBox").length);
-            if($(document.getElementById('#aboutBox').style.display == 'none')){
+            if(document.getElementById('aboutBox').style.display == 'none'){
 
                 selector.css("display", "inline");
             }
