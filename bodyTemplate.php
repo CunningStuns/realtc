f8364d1b31466fa91b966b84b2fa7f9de471f71d
Vatsalya Goel <vatsalyagoel@Macs-MacBook-Pro.local>
2015-08-15 19:45:12 +1000

    latest git changes'

--- bodyTemplate.php
+++ bodyTemplate.php
@@ -9,22 +9,25 @@ function bodyTemplate( $mainpanel, $sidepanel){
 <div id='content'>
 	<div id='main'>
 		<table id='main'>
-      
 	    <tr>
-	      <th>";
+	      <th><!--Empty Table Headers, since we won't need these --></th>
+	      <th></th>
+      </tr>
+      <tr>
+	      <td id='mainview'>";
 	echo $mainpanel; // Main Control View
-	echo "</th>
-	      <th>";
+	echo "</td>
+	      <td id='sideview'>";
   echo $sidepanel; // Side bar which will be used for either signing in/up, and then for showing who is part of the workspace.
-  echo "</th>
+  echo "</td>
 	    </tr>
-	    
 		</table>
 	</div>
 </div>
 
 <div id='footer'>
-	
+	<p>Copyright &copy; 2015</p>
+	<p>A product of the Cunning Stunts during <a href='http://unihack.net/'>UNIHACK 2015</a></p>
 </div>
 </body>
 
@@ -32,13 +35,26 @@ function bodyTemplate( $mainpanel, $sidepanel){
 }
 
 function bodyTemplate2($mainPanel, $mainCSS, $sidePanel, $sideCSS){
-	echo "	<table style='width:100%' border='1' style='width:100%'>
-  				<tr style=' ".$mainCSS."'>
-					<td colspan='2'>$mainPanel</td>
-  				</tr>
-  				<tr style=' ".$sideCSS."'>
-					<td>$sidePanel</td>
-  				</tr>
-</table> ";
+	echo "
+		<body>
+		  	<div id='headBar'>
+				<a><img src='realTCLogo.jpg' alt='imgnotfound.jpg' /></a>
+			</div>
+		
+			<div id='content'>
+				<table style='width:100%'  style='width:100%'>
+						<tr id = main style=' ".$mainCSS."'>
+							<td colspan='2'>$mainPanel</td>
+						</tr>
+						<tr style=' ".$sideCSS."'>
+							<td>$sidePanel</td>
+						</tr>
+			</table> 
+			
+			<div id='footer'>
+			
+		</div>
+	</body>
+	";
 }
 ?>
