0271428f35eb36ebf7b1646301fa5efc611cfdf2
Vatsalya Goel <vatsalyagoel@Macs-MacBook-Pro.local>
2015-08-16 10:42:23 +1000

    added new logo

--- musicStreaming/index.php
+++ musicStreaming/index.php
@@ -9,7 +9,7 @@
 //error_reporting(-1);
 //ini_set('display_errors', 'On');
 
-$path = $_SERVER["DOCUMENT_ROOT"]."\\upload\\";
+$path = $_SERVER["DOCUMENT_ROOT"]."/upload/";
 $files = scandir($path);
 echo $files;
 
