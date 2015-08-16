db35aef88ecb0123a7ba6733cb619d3acff63d8b
Vatsalya Goel <vatsalyagoel@Macs-MacBook-Pro.local>
2015-08-16 10:12:01 +1000

    added new logo

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 // make a folder upload to move your file.I yhink this code is necessary to modified but right now it working correctly.
 if(isset($_POST['submit']))
 {
-$allowedExts = array("jpg", "jpeg", "gif", "png", "mp3", "mp4", "wma");
+$allowedExts = array("jpg", "jpeg", "gif", "png", "mp3", "mp4");
 //echo $extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
 $fileName = $_FILES['file']['name'];
 $extension = substr($fileName, strrpos($fileName, '.') + 1); // getting the info about the image to get its extension
@@ -28,7 +28,7 @@ if(in_array($extension, $allowedExts))
       }
     else
       {
-       move_uploaded_file($_FILES["file"]["tmp_name"],"upload/" . $_FILES["file"]["name"]);
+       move_uploaded_file($_FILES["file"]["tmp_name"],"\\realtc\\upload\\" . $_FILES["file"]["name"]);
       echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
       }
     }
